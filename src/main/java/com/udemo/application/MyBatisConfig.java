1d7f4bd291f85927764f9a5937128ab756781afa
韩世豪 <hsh@wdtianxia.com>
2017-12-12 17:10:11 +0800

    springBoot 整合mybatis:添加Spring 集成方式

---
+++ src/main/java/com/udemo/application/MyBatisConfig.java
@@ -0,0 +1,72 @@
+package com.udemo.application;
+
+import com.github.pagehelper.PageHelper;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+
+/**
+ * Desc: Mybatis 非SpringBoot官方集成（手动版）
+ * User: hansh
+ * Date: 2017/12/12
+ * Time: 16:59
+ */
+//@Configuration
+//@EnableTransactionManagement
+public class MyBatisConfig  /*implements TransactionManagementConfigurer*/{
+
+//    @Autowired
+//    private DataSource dataSource;
+//
+//
+//    @Bean(name = "sqlSessionFactory")
+//    public SqlSessionFactory sqlSessionFactoryBean() {
+//        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+//        bean.setDataSource(dataSource);
+//        bean.setTypeAliasesPackage("com.udemo.model");
+//
+//        //分页插件
+//        PageHelper pageHelper = new PageHelper();
+//        Properties properties = new Properties();
+//        properties.setProperty("reasonable", "true");
+//        properties.setProperty("supportMethodsArguments", "true");
+//        properties.setProperty("returnPageInfo", "check");
+//        properties.setProperty("params", "count=countSql");
+//        pageHelper.setProperties(properties);
+//
+//        //添加插件
+//        bean.setPlugins(new Interceptor[]{pageHelper});
+//
+//        //添加XML目录
+//        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+//        try {
+//            bean.setMapperLocations(resolver.getResources("classpath:mapping/*.xml"));
+//            return bean.getObject();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            throw new RuntimeException(e);
+//        }
+//    }
+//    @Bean
+//    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+//        return new SqlSessionTemplate(sqlSessionFactory);
+//    }
+//    @Bean
+//    @Override
+//    public PlatformTransactionManager annotationDrivenTransactionManager() {
+//        return null;
+//    }
+}
---
+++ src/main/java/com/udemo/application/MyBatisMapperScannerConfig.java
@@ -0,0 +1,25 @@
+package com.udemo.application;
+
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Desc: MyBatis扫描接口
+ * User: hansh
+ * Date: 2017/12/12
+ * Time: 17:07
+ */
+//@Configuration
+//@AutoConfigureAfter(MyBatisConfig.class)
+public class MyBatisMapperScannerConfig {
+
+//    @Bean
+//    public MapperScannerConfigurer mapperScannerConfigurer() {
+//        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+//        mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
+//        mapperScannerConfigurer.setBasePackage("com.udemo.mapper");
+//        return mapperScannerConfigurer;
+//    }
+}
