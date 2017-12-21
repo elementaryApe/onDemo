a0af51ba3a606c46ffb3994bdc565940a3b0450b
韩世豪 <hsh@wdtianxia.com>
2017-12-21 16:49:48 +0800

    1.添加javaMail发送邮件功能

--- src/main/java/com/udemo/controller/IndexController.java
+++ src/main/java/com/udemo/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.udemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class IndexController {
+    @Autowired
+    private JavaMailSenderImpl mailSender;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
-        return "just only one demo ";
+//        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+//        // 设置收件人，寄件人
+//        simpleMailMessage.setTo(new String[] {"563272313@qq.com"});
+//        simpleMailMessage.setFrom("hshandzmq@163.com");
+//        simpleMailMessage.setSubject("Spring Boot Mail 邮件测试【文本】");
+//        simpleMailMessage.setText("这里是一段简单文本。");
+//        // 发送邮件
+//        mailSender.send(simpleMailMessage);
+//        System.out.println("邮件已发送");
+        return "邮件已发送";
     }
 
 }
